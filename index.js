45af857a02745b43ef32335d8957a2a31194c0d8
Nico <nicopasquariello@gmail.com>
2024-02-28 18:16:49 -0700

    added endpoint for '/api/movies'

--- index.js
+++ index.js
@@ -3,10 +3,19 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
+const movies = [
+    {id:1, name:"movie1"},
+    {id:2, name:"movie2"},
+    {id:3, name:"movie3"}
+]
+
 app.get('/', (req, res) => {
     res.send('hello world')
 })
 
+app.get('/api/movies', (req, res) => {
+    res.send(movies)
+})
 
 app.listen(port, (req, res) => {
     console.log(`listening on port ${port}...`)
