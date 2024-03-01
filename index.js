9af150bf5f30390eeab0991dc3a0d33045ce181f
nicopasq <90681300+nicopasq@users.noreply.github.com>
2024-02-29 23:49:41 -0700

    added DELETE endpoint

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 //Manage list of genres
 //Get all genres √
 //Create a genre √
-//Update a genre
-//Delete a genre
+//Update a genre √
+//Delete a genre √
 
 const Joi = require('joi')
 const express = require('express')
@@ -23,6 +23,24 @@ const genres = [
     {id:3, genre:"genre3"}
 ]
 
+
+function validateGenre(genre){
+    const schema = Joi.object({
+        genre:Joi.string().min(3).required()
+    })
+
+    return schema.validate(genre)
+}
+
+function findGenre(genreId, res){
+    const genre = genres.find(g => g.id === genreId)
+    if (!genre){
+        return res.status(404).send('Genre could not be found.')
+    } else {
+        return genre
+    }
+}
+
 app.get('/', (req, res) => {
     res.send('Welcome to Vidly App')
 })
@@ -31,8 +49,7 @@ app.get('/api/movies/genres', (req, res) => {
     res.send(genres)
 })
 app.get('/api/movies/genres/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id))
-    if (!genre) return res.status(404).send("Genre could not be found")
+    const genre = findGenre(parseInt(req.params.id), res)
     res.send(genre)
 })
 
@@ -52,8 +69,7 @@ app.post('/api/movies/genres', (req, res) => {
 })
 
 app.put('/api/movies/genres/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id))
-    if (!genre) return res.status(404).send('Genre could not be found.')
+    const genre = findGenre(parseInt(req.params.id), res)
     const {error, value}= validateGenre(req.body)
     if(error){
         return res.status(422).send(error.details[0].message)
@@ -62,10 +78,11 @@ app.put('/api/movies/genres/:id', (req, res) => {
     res.send({genre:genre, message:'Successfully updated genre.'})
 })
 
-function validateGenre(genre){
-    const schema = Joi.object({
-        genre:Joi.string().min(3).required()
-    })
+app.delete('/api/movies/genres/:id', (req, res) => {
+    const genre = findGenre(parseInt(req.params.id), res)
 
-    return schema.validate(genre)
-}
+    const index = genres.indexOf(genre)
+    genres.splice(index, 1)
+
+    res.status(200).send("Successfully deleted genre.")
+})
